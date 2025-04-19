ea86ef052a20e76cb353178749aa5833c195e2f5
Edith Agai <email.02yt@gmail.com>
2025-04-19 13:45:19 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const names = ["Edith","Cole","Sandra"]
+
+function writeCards(names, birthDay) {
+    let birthDaymessages = [];
+    for (let i = 0; i < names.length; i++) {
+      birthDaymessages.push(`Thank you, ${names[i]}, for the wonderful ${birthDay} gift!`);
+    }
+    return birthDaymessages;
+  }
+  
+function countDown(Number) {
+    while (Number >= 0) {
+      console.log(Number);
+      Number--;
+    }
+  }
+  
